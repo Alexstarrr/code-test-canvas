64161cbe7ea0c6e986a19fe941c21c70c6be1c83
Alexstarrr <yxyyxy417@gmail.com>
2021-05-19 16:52:47 +1000

    color starts from center

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
 import React, { useEffect, useRef } from 'react';
 import './App.css';
 
+const rgbColorLoop = () => {
+  let rgbColor = [];
+  for (let i = 8; i <= 256; i += 8){
+    for (let j = 8; j <= 256; j += 8){
+      for (let k = 8; k <= 256; k += 8){
+          rgbColor.push([i,j,k])
+      }
+    }
+  }
+  return rgbColor;
+}
+
 function App() {
   const canvasRef = useRef(null);
-  // const ctx = canvas.getContext("2d");
+
   useEffect(() => {
-    // const rgbColorLoop = () => {
-    //   let rgbColor = [];
-    //   for (let i = 8; i <= 256; i += 8){
-    //     for (let j = 8; j <= 256; j += 8){
-    //       for (let k = 8; k <= 256; k += 8){
-    //           rgbColor.push([i,j,k])
-    //       }
-    //     }
-    //   }
-    //   // rgbColor = rgbColor.slice(0,4000);
-    //   return rgbColor;
-    // }
-    // const oldrgbColor = rgbColorLoop();
-    // setPixelColor(rgbColorLoop());
-    // console.log(canvasRef);
+    const allColors = rgbColorLoop();
+    const closedPosList = [];
+    const openPosList = [];
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d')
-    let rgbColor = [];
-    let index = 0;
-    for (let i = 8; i <= 256; i += 8){
-      for (let j = 8; j <= 256; j += 8){
-        for (let k = 8; k <= 256; k += 8){
-            rgbColor.push([i,j,k])
-            ctx.fillStyle= `rgb(${i},${j},${k}`;
-            ctx.fillRect(index%256, Math.floor(index/256), 1, 1);
-            index ++ ;
+    const ctx = canvas.getContext('2d');
+    // Check if position exist in array
+    const checkPosition = (x,y,pArray) => {
+      for(const position of pArray) {
+        if (position[0] === x && position[1] === y)
+          return  true;
+        else
+          continue;
+      }
+    }
+    
+    const fillPosWithColor = () => {
+      const getPosAroundCenter = (x,y) => [[x-1,y],[x-1,y-1],[x,y-1],[x+1,y-1],[x+1,y],[x+1,y+1],[x,y+1],[x-1,y+1]];
+      // starts the position array with center point
+      openPosList.push([128,64]);
+      while (closedPosList.length < allColors.length) {
+        // pop out the checked position from open list 
+        const centerPoint = openPosList.shift();
+        // push the checked position to closed list
+        closedPosList.push(centerPoint);
+        const currentColor = allColors[closedPosList.indexOf(centerPoint)]
+        // Start fill
+        ctx.fillStyle = `rgb(${currentColor[0]},${currentColor[1]},${currentColor[2]})`;
+        ctx.fillRect(centerPoint[0], centerPoint[1], 1, 1);    
+        const x = centerPoint[0];
+        const y = centerPoint[1];
+        const posAroundCenter = getPosAroundCenter(x,y);
+        for (const pos of posAroundCenter) {
+          if (!checkPosition(pos[0],pos[1],openPosList) && !checkPosition(pos[0],pos[1],closedPosList) && pos[0]>=0 && pos[1]>=0 && pos[0]<=256 && pos[1]<=128)
+          openPosList.push([pos[0],pos[1]]);
         }
       }
     }
-      // ctx.fillStyle= `rgb(${el[0]},${el[1]},${el[2]}`;
-      // ctx.fillRect(index%256, Math.floor(index/256), 1, 1);
 
-    // ctx.fillStyle = "rgb(256,8,256)";
-    // ctx.fillRect(1, 0, 1, 1);
-    // console.log(pixelColor);
+    fillPosWithColor();
   },[])
 
   return (
@@ -48,7 +64,6 @@ function App() {
         ref={canvasRef}
         width={256}
         height={128}
-        data-testid="canvas"
       />   
     </div>
   );
